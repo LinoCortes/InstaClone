aba39aa0511c8be2b1d3c303a6791f4f22ecebe4
Lino Cortes <lino.cortess@gmail.com>
2024-01-13 15:23:36 -0300

    Finish design auth page
    Design for the auth page has been done, we'll move with the sidebar of the home page next

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -6,18 +6,20 @@ export const AuthPage = () => {
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxW={"container.md"} padding={0}>
         {/* Lado izquierdo */}
-        <Box display={{ base: "none", md: "block" }}>
-            <Image src="/auth.png" h={650} alt="Phone img"/>
-        </Box>
-        {/* Lado derecho */}
-        <VStack spacing={4} align={"stretch"}>
-            <AuthForm/>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="Phone img" />
+          </Box>
+          {/* Lado derecho */}
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
             <Box textAlign={"center"}>Get the app</Box>
             <Flex gap={5} justifyContent={"center"}>
-                <Image src="/playstore.png" h={"10"} alt="Playstore logo"/>
-                <Image src="/microsoft.png" h={"10"} alt="Microsoft logo"/>
+              <Image src="/playstore.png" h={"10"} alt="Playstore logo" />
+              <Image src="/microsoft.png" h={"10"} alt="Microsoft logo" />
             </Flex>
-        </VStack>
+          </VStack>
+        </Flex>
       </Container>
     </Flex>
   );
