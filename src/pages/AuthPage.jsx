7b09749655f74fb09e530008279e21e7c215373e
Lino Cortes <lino.cortess@gmail.com>
2024-01-12 23:49:30 -0300

    Designing auth page
    Designing auth page along with routers and other content related to general display

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,24 @@
+import { Container, Flex, Box, Image, VStack } from "@chakra-ui/react";
+import { AuthForm } from "../components/AuthForm";
+
+export const AuthPage = () => {
+  return (
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        {/* Lado izquierdo */}
+        <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="Phone img"/>
+        </Box>
+        {/* Lado derecho */}
+        <VStack spacing={4} align={"stretch"}>
+            <AuthForm/>
+            <Box textAlign={"center"}>Get the app</Box>
+            <Flex gap={5} justifyContent={"center"}>
+                <Image src="/playstore.png" h={"10"} alt="Playstore logo"/>
+                <Image src="/microsoft.png" h={"10"} alt="Microsoft logo"/>
+            </Flex>
+        </VStack>
+      </Container>
+    </Flex>
+  );
+};
