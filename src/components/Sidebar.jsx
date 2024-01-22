94be4dfa05b287c859dc42713b6b6c8374ad4375
Lino Cortes <lino.cortess@gmail.com>
2024-01-22 14:19:47 -0300

    Update Sidebar
    Sidebar fully implemented, next is the home page after login

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,40 @@
-import { Avatar, Box, Flex, Link } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Link, Tooltip } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
-import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from "../assets/constants";
-import {AiFillHome} from "react-icons/ai"
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from "../assets/constants";
+import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 export const Sidebar = () => {
-  
   const sidebarItems = [
     {
-      icon:<AiFillHome size={25}/>,
-      text:"Home",
-      link:"/"
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
     },
     {
-      icon:<SearchLogo/>,
-      text:"Home",
+      icon: <SearchLogo />,
+      text: "Home",
     },
     {
-      icon:<NotificationsLogo/>,
-      text:"Notifications",
+      icon: <NotificationsLogo />,
+      text: "Notifications",
     },
     {
-      icon:<CreatePostLogo/>,
-      text:"Create",
+      icon: <CreatePostLogo />,
+      text: "Create",
     },
     {
-      icon:<Avatar size={"sm"} name="Lino Cortés" src="/profilepic.png"/>,
-      text:"Home",
+      icon: <Avatar size={"sm"} name="Lino Cortés" src="/profilepic.png" />,
+      text: "Profile",
     },
-  ]
-  
+  ];
+
   return (
     <Box
       height={"100vh"}
@@ -48,7 +54,7 @@ export const Sidebar = () => {
           display={{ base: "none", md: "block" }}
           cursor={"pointer"}
         >
-          <InstagramLogo/>
+          <InstagramLogo />
         </Link>
         <Link
           to={"/"}
@@ -57,11 +63,65 @@ export const Sidebar = () => {
           display={{ base: "block", md: "none" }}
           cursor={"pointer"}
           borderRadius={6}
-          _hover={{bg:"whiteAlpha.200"}}
-          w={10}
+          _hover={{ bg: "whiteAlpha.200" }}
+          w={{ base: 10, md: "full" }}
         >
-          <InstagramMobileLogo/>
+          <InstagramMobileLogo />
         </Link>
+        <Flex direction={"column"} gap={5} cursor={"pointer"}>
+          {sidebarItems.map((item, index) => (
+            <Tooltip
+              key={index}
+              hasArrow
+              label={item.text}
+              placement="right"
+              ml={1}
+              openDelay={500}
+              display={{ base: "block", md: "none" }}
+            >
+              <Link
+                display={"flex"}
+                to={item.link || null}
+                as={RouterLink}
+                alignItems={"center"}
+                gap={4}
+                _hover={{ bg: "whiteAlpha.400" }}
+                borderRadius={6}
+                p={2}
+                w={{ base: "full" }}
+                justifyContent={{ base: "center", md: "flex-start" }}
+              >
+                {item.icon}
+                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
+              </Link>
+            </Tooltip>
+          ))}
+        </Flex>
+        <Tooltip
+          hasArrow
+          label="Logout"
+          placement="right"
+          ml={1}
+          openDelay={500}
+          display={{ base: "block", md: "none" }}
+        >
+          <Link
+            display={"flex"}
+            to="/auth"
+            as={RouterLink}
+            alignItems={"center"}
+            gap={4}
+            _hover={{ bg: "whiteAlpha.400" }}
+            borderRadius={6}
+            p={2}
+            w={{ base: "full" }}
+            justifyContent={{ base: "center", md: "flex-start" }}
+            mt={"auto"}
+          >
+            <BiLogOut size={25}/>
+            <Box display={{ base: "none", md: "block" }}>Logout</Box>
+          </Link>
+        </Tooltip>
       </Flex>
     </Box>
   );
